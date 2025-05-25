3733969de5dcd9be13aaa041f22abf9adc32da55
kochp1 <juanandreskochp@gmail.com>
2025-05-25 19:07:57 -0400

    Implement dark mode toggle functionality and update styles for dark mode

---
+++ js/darkMode.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        enableDarkMode();
+    }
+
+    darkModeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            enableDarkMode();
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            disableDarkMode();
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    });
+
+    function enableDarkMode() {
+        document.body.classList.add('dark-mode');
+        const icon = darkModeToggle.querySelector('i');
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+
+        const nav = document.querySelectorAll(".nav");
+        nav.forEach(nav => nav.classList.add("dark-mode"));
+    }
+
+    function disableDarkMode() {
+        document.body.classList.remove('dark-mode');
+        const icon = darkModeToggle.querySelector('i');
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+
+        const nav = document.querySelectorAll(".nav");
+        nav.forEach(nav => nav.classList.remove("dark-mode"));
+
+    }
+});
