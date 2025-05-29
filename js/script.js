52c98e9c3e867c4e5281f86f1a339eeba7e7100b
kochp1 <juanandreskochp@gmail.com>
2025-05-29 18:06:33 -0400

    Refactor form validation to ensure email, subject, and message lengths are properly checked before submission

--- js/script.js
+++ js/script.js
@@ -20,6 +20,21 @@ document.addEventListener("DOMContentLoaded", () => {
         const asunto = form.asunto.value.trim();
         const mensaje = form.mensaje.value.trim();
 
+        if (email.length > 50) {
+            console.log('El email puede tener un máximo de 50 caracteres')
+            return;
+        }
+
+        if (asunto.length > 70) {
+            console.log('El asunto puede tener un máximo de 70 caracteres')
+            return;
+        }
+
+        if (mensaje.length > 50) {
+            console.log('El mensaje puede tener un máximo de 1000 caracteres')
+            return;
+        }
+
         console.log(email);
         console.log(asunto);
         console.log(mensaje);
